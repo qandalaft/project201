5cde047dc48ed1f59198d9e248a84f182d62e930
qandalaft <qandalaftemad@gmail.com>
2021-07-30 15:54:45 +0300

    re do the guess game agine

--- js/app.js
+++ js/app.js
@@ -1,70 +1,40 @@
 'use strict'
+alert("welcome to my first project");
 
-alert("welcome to my first project")
+let userName = prompt("can you tell us your name?");
+alert(`welcome ${userName}`);
 
-let uesrname = prompt("can you tell us your name? ")
 
-alert("welcome " + uesrname)
-
-let age = prompt(" now your age catagory if your age are between 18-24 print Y and if your age between 25-30 print N? ").toLowerCase()
-switch (age) {
-    case "y":
-        alert("your are very young enjoy your time  ")
+let guessNumber = Number(prompt(`Now ${userName} try to guess a number between 1 to 5  you have only just 4 attempts`));
+for (let i = 0; i < 3; i++) {
+    if (guessNumber === 3) {
+        alert('great this is the rigth answer');
+        //console.log(guessNumber);
         break;
-    case "n":
-        alert("you are young too but you must think about your futur")
-        break;
-
-}
-let animeLover = prompt("are you Anime lover answer by yes or no?").toUpperCase()
-switch (animeLover) {
-    case "NO":
-        alert("you must try to watch one I recommand (Hunter X Hunter) its amazing I put a link to you scroll down to see it")
-        break;
-    case "YES":
-        let hunterLover = prompt("are you Hunter X hunter lover yes or no ").toLowerCase()
-        break;
-    default:
-        alert("you can't enter this Just yes or no ")
-}
-
-switch (hunterLover) {
-    case "yes":
-        alert("great you are a real Anime lover ")
-        break;
-    case "no":
-        alert("no you are not Anime lover")
-        break;
-    default:
-        alert("you can't enter this Just yes or no ")
-}
-
-
-alert('welcome again to my site and ready to the gussing game so Lets start......')
-let startGame = prompt('now please if you want to start the game now Enter yes')
-
-if (startGame == 'yes') {
-
-    alert('now you must Enter a number between  1 and 10 , you have only 4 trys ')
-    let answer = 7;
-    let guess = prompt('here Enter your answer')
-
-    for (let i = 1; i < 4; i++) {
-        if (answer == guess) {
-            alert('good job');
-            break;
-
-        } else {
-            guess = prompt('try agian')
-        }
-
-
     }
+    else if (guessNumber === 1) {
+        alert('too low');
+        //console.log(guessNumber);
+        guessNumber = Number(prompt(`Now ${userName} try to guess a number between 1 to 5 and remember you have just 4 attempts`));
+    } else if (guessNumber === 2) {
+        alert('too low');
+        //console.log(guessNumber);
+        guessNumber = Number(prompt(`Now ${userName} try to guess a number between 1 to 5 and remember you have just 4 attempts`));
+    } else if (guessNumber === 4) {
+        alert('too high');
+        // console.log(guessNumber);
+        guessNumber = Number(prompt(`Now ${userName} try to guess a number between 1 to 5 and remember you have just 4 attempts`));
+    }
+    else if (guessNumber === 5) {
+        alert('too high');
+        // console.log(guessNumber);
+        guessNumber = Number(prompt(`Now ${userName} try to guess a number between 1 to 5 and remember you have just 4 attempts`));
+    }
+}
 
 
-}
-else {
-    alert('come back next time to play')
-}
+//alert('The correct answer is 3');
+
+
 
 
